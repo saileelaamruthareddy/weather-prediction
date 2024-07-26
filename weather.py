7cef7cd3162d2b35a594c4fef635bfb1f902bd0c
saileelaamruthareddy <168632089+saileelaamruthareddy@users.noreply.github.com>
2024-07-26 14:27:03 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -46,6 +46,16 @@ if st.button("Submit"):
     })
     weather=model.predict(d)[0]
     st.write(weather)
+    if weather =='Rainy':
+        st.image(r"C:\Users\saile\OneDrive\Desktop\rainy.jpeg",width=1000)
+    elif weather=='Cloudy':
+        st.image(r"C:\Users\saile\OneDrive\Desktop\cloudy.jpeg",width=1000)
+    elif weather=='Sunny':
+        st.image(r"C:\Users\saile\OneDrive\Desktop\sunny.jpeg",width=1000)
+    elif weather=='Snowy':
+        st.image(r"C:\Users\saile\OneDrive\Desktop\snowy.jpeg",width=700)
+     
+
      
 
     
